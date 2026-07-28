4eb9e50486ec5f670801d67222e8fc8949f5457b
Your Name <you@example.com>
2026-07-28 14:09:59 +0300

    Add dockerfile

---
+++ src/app/components/auth/modal.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import styles from "./modal.module.scss";
+
+const modalComponent: FC = () => {
+    return (
+        <div className={styles.modal}>
+
+        </div>
+    )
+}
+
+export default modalComponent
